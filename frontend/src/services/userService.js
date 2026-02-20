654382fbd6fa70eee092376096f377adaf9c9b3a
JahnaviNalluri <158235481+JahnaviNalluri@users.noreply.github.com>
2026-02-20 19:59:21 +0530

    Update userService API endpoints to include '/user'

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,20 +1,20 @@
 import API from "../api/axios";
 
 export const getUserProfile=async(id)=>{
-    const {data}=await API.get(`/${id}`);
+    const {data}=await API.get(`/user/${id}`);
     return data;
 };
 
 export const updateUser=async(id,formData)=>{
-    const {data}=await API.put(`/${id}`,formData);
+    const {data}=await API.put(`/user/${id}`,formData);
     return data;
 };
 
 export const deleteUser=async(id)=>{
-    const {data}=await API.delete(`/${id}`);
+    const {data}=await API.delete(`/user/${id}`);
     return data;
 };
 export const getAllUsers=async()=>{
-    const {data}=await API.get("/");
+    const {data}=await API.get("/user");
     return data;
 }
