b28dfdd92209a2f2a95052d55712fb71c5872698
JahnaviNalluri <158235481+JahnaviNalluri@users.noreply.github.com>
2026-02-20 20:11:31 +0530

    Update API endpoints for user login and registration

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,11 +1,11 @@
 import API from "../api/axios";
 
 export const loginUser=async(formData)=>{
-    const {data} =await API.post("/user/login",formData);
+    const {data} =await API.post("/api/user/login",formData);
     localStorage.setItem("userInfo",JSON.stringify(data));
     return data;
 };
 export const  registerUser=async(formData)=>{
-    const {data} =await API.post("/user",formData);
+    const {data} =await API.post("/api/user",formData);
     return data;
 };
